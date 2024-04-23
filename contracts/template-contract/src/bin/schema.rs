45b6d5011d312d8f4d43c86c4b75cdb8e958ea25
Amit Prasad <mail@amitprasad.dev>
2024-04-23 12:24:49 -0500

    Initialize template

---
+++ contracts/template-contract/src/bin/schema.rs
@@ -0,0 +1,11 @@
+use cosmwasm_schema::write_api;
+
+use template_interfaces::template::{ExecuteMsg, InstantiateMsg, QueryMsg};
+
+fn main() {
+    write_api! {
+        instantiate: InstantiateMsg,
+        execute: ExecuteMsg,
+        query: QueryMsg,
+    }
+}
